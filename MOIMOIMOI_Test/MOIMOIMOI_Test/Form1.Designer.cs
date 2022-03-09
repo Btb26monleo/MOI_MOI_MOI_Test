0fce2e0a9ff4dbded83e8195ab19c74020628ff7
ntcthong <ntcthong.it@gmail.com>
2022-03-09 15:44:56 +0700

    chào các bạn

--- MOIMOIMOI_Test/MOIMOIMOI_Test/Form1.Designer.cs
+++ MOIMOIMOI_Test/MOIMOIMOI_Test/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace MOIMOIMOI_Test
             this.label2 = new System.Windows.Forms.Label();
             this.textboxpassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -49,7 +50,7 @@ namespace MOIMOIMOI_Test
             // textboxusername
             // 
             this.textboxusername.Location = new System.Drawing.Point(199, 92);
-            this.textboxusername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxusername.Margin = new System.Windows.Forms.Padding(4);
             this.textboxusername.Name = "textboxusername";
             this.textboxusername.Size = new System.Drawing.Size(456, 22);
             this.textboxusername.TabIndex = 1;
@@ -67,7 +68,7 @@ namespace MOIMOIMOI_Test
             // textboxpassword
             // 
             this.textboxpassword.Location = new System.Drawing.Point(199, 156);
-            this.textboxpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textboxpassword.Margin = new System.Windows.Forms.Padding(4);
             this.textboxpassword.Name = "textboxpassword";
             this.textboxpassword.Size = new System.Drawing.Size(456, 22);
             this.textboxpassword.TabIndex = 1;
@@ -75,7 +76,7 @@ namespace MOIMOIMOI_Test
             // btnLogin
             // 
             this.btnLogin.Location = new System.Drawing.Point(356, 210);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(100, 28);
             this.btnLogin.TabIndex = 2;
@@ -83,17 +84,28 @@ namespace MOIMOIMOI_Test
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(809, 379);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.textboxpassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textboxusername);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -108,6 +120,7 @@ namespace MOIMOIMOI_Test
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textboxpassword;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- MOIMOIMOI_Test/MOIMOIMOI_Test/Form1.cs
+++ MOIMOIMOI_Test/MOIMOIMOI_Test/Form1.cs
@@ -21,5 +21,10 @@ namespace MOIMOIMOI_Test
         {
             MessageBox.Show("Chào các bạn");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
